fac5e9b64ba404b4de838dccaa8f483c18b166c2
Jalil Hashemi <jalil.hashemi@students.fhnw.ch>
2018-05-16 09:24:03 +0200

    hover is added on line chart

--- src/index.js
+++ src/index.js
@@ -26,11 +26,19 @@
 //     });
 // });
 
-var svg = d3.select("svg"),
-    margin = { top: 20, right: 20, bottom: 30, left: 50 },
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var margin = { top: 20, right: 50, bottom: 30, left: 50 },
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var svg = d3.select("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+bisectDate = d3.bisector(function (d) { return d.year; }).left,
+    formatValue = d3.format(",.1f"),
+    formatPercent = function (d) { return "%" + formatValue(d); };
 
 var x = d3.scaleLinear()
     .rangeRound([0, width]);
@@ -39,7 +47,7 @@ var y = d3.scaleLinear()
     .rangeRound([height, 0]);
 
 var line = d3.line()
-    .x(function (d) { return x(d.year + 6); })
+    .x(function (d) { return x(d.year); })
     .y(function (d) { return y(d.percent); });
 
 d3.csv("./data/resident-population-of-Zurich.csv", function (d) {
@@ -52,7 +60,8 @@ d3.csv("./data/resident-population-of-Zurich.csv", function (d) {
     x.domain(d3.extent(data, function (d) { return d.year; }));
     y.domain(d3.extent(data, function (d) { return d.percent; }));
     xaxis = d3.axisBottom().tickFormat(d3.format(".0f")).scale(x);
-    g.append("g")
+
+    svg.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(xaxis)
         .append("text")
@@ -63,7 +72,7 @@ d3.csv("./data/resident-population-of-Zurich.csv", function (d) {
         .attr("text-anchor", "start")
         .text("years");
 
-    g.append("g")
+    svg.append("g")
         .call(d3.axisLeft(y))
         .append("text")
         .attr("fill", "#000")
@@ -93,9 +102,43 @@ d3.csv("./data/resident-population-of-Zurich.csv", function (d) {
             .duration(6000)
             .ease(d3.easeLinear)
             .attr("stroke-dashoffset", 0);
+
+        var focus = svg.append("g")
+            .attr("class", "focus")
+            .style("display", "none");
+
+        focus.append("circle")
+            .attr("r", 4.5)
+            .attr("fill", "steelblue")
+            .attr("stroke", "steelblue");
+
+        focus.append("text")
+            .attr("x", 9)
+            .attr("dy", ".35em");
+
+        svg.append("rect")
+            .attr("class", "overlay")
+            .attr("fill", "none")
+            .attr("pointer-events", "all")
+            .attr("width", width)
+            .attr("height", height)
+            .on("mouseover", function () { focus.style("display", null); })
+            .on("mouseout", function () { focus.style("display", "none"); })
+            .on("mousemove", mousemove);
+
+        function mousemove() {
+            var x0 = x.invert(d3.mouse(this)[0]),
+                i = bisectDate(data, x0, 1),
+                d0 = data[i - 1],
+                d1 = data[i],
+                d = x0 - d0.year > d1.year - x0 ? d1 : d0;
+            focus.attr("transform", "translate(" + x(d.year) + "," + y(d.percent) + ")");
+            focus.select("text").text(formatPercent(d.percent));
+        }
     });
 
     d3.select("#reset").on("click", function () {
         d3.select(".line").remove();
+        d3.select(".overlay").remove();
     });
 });
